23aa1afecae87169d070258d4d8185fd2eac2fcb
Simon <65659502+Jebeiuk@users.noreply.github.com>
2021-07-29 12:03:53 +0100

    Add files via upload

---
+++ libs/php/getPostalcodesInfo.php
@@ -0,0 +1,28 @@
+<?php
+
+	$executionStartTime = microtime(true) / 1000;
+
+	$url='http://api.geonames.org/findNearbyPostalCodesJSON?formatted=true&postalcode=' . $_REQUEST['postalcode'] . '&country=' . $_REQUEST['country'] . '&radius=' . $_REQUEST['radius'] . '&username=sgaravan';
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL,$url);
+
+	$result=curl_exec($ch);
+
+	curl_close($ch);
+
+	$decode = json_decode($result,true);	
+
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "mission saved";
+	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+	$output['data'] = $decode['geonames'];
+	
+	header('Content-Type: application/json; charset=UTF-8');
+
+	echo json_encode($output); 
+
+?>
---
+++ libs/php/getWeatherInfo.php
@@ -0,0 +1,28 @@
+<?php
+
+	$executionStartTime = microtime(true) / 1000;
+
+	$url= 'http://api.geonames.org/weatherInfoJSON?formatted=true&north=' . $_REQUEST['north'] . '&south=' . $_REQUEST['south'] .'&east='.$_REQUEST['east'].'&west='.$_REQUEST['west']. '&username=sgaravan';
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL,$url);
+
+	$result=curl_exec($ch);
+
+	curl_close($ch);
+
+	$decode = json_decode($result,true);	
+
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "mission saved";
+	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+	$output['data'] = $decode['geonames'];
+	
+	header('Content-Type: application/json; charset=UTF-8');
+
+	echo json_encode($output); 
+
+?>
